74a6250442c5ffbc986f900947c6c17dfac2be6c
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-06-17 16:24:56 -0500

    Bootstrap y estilos iniciales

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-persons",
-  templateUrl: "./persons.component.html"
+  templateUrl: "./persons.component.html",
+  styleUrls: ["./persons.component.css"]
 })
 
 export class PersonsComponent{
