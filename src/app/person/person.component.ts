ca4fd074d388daeb633460730c69e576a03b68f0
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-06-17 16:33:57 -0500

    Interpolación

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -3,10 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
+  styleUrls: ['./person.component.css'],
 })
 export class PersonComponent {
-
-  constructor() { }
-
+  name: string = 'Álvaro';
+  lastname: string = 'Barrera';
+  year: number = 21;
+  // VARIABLES PRIVADAS
+  // private year: number = 21;
+  // getEdad(): number{
+  //   return this.year;
+  // }
 }
