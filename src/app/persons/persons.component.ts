24da88e5edcc9060a59e81b5b323dd6e018f0b48
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-06-18 09:56:03 -0500

    Two way binding

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -8,7 +8,11 @@ import { Component } from '@angular/core';
 export class PersonsComponent {
   isDisabled = false;
   message = 'Ninguna persona agregada';
+  title = '';
   storePerson() {
     this.message = 'Persona agregada';
   }
+  // editTitle(event: Event) {
+  //   this.title = (<HTMLInputElement>event.target).value;
+  // }
 }
